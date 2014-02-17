c219b0383d8c78664758e85a6aa12171cdca9ac3
Hsi Yi Hsuan <hsi.yihsuan@gmail.com>
2014-02-17 13:36:45 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picteresting::Application.config.secret_key_base = '86129316d0d105411eecb7252c547133e82d856277bb1e8438f20d9368ed25f01f2cbf883d94c1385cdf48513f80d826b6d82f7a767ad14ef699f7f82491fbda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Picteresting::Application.config.session_store :cookie_store, key: '_Picteresting_session'
